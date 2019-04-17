19e86469503325b18a89aea9262c233367fb0f17
Nipun Haldar <nipunhaldar24@gmail.com>
2019-04-18 00:09:49 +0530

    implemented react-router, styled links

---
+++ src/Quotes.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class Quotes extends Component {
+    render() {
+        return(
+            <div>
+                quotes
+            </div>
+        );
+    }
+}
+
+export default Quotes
